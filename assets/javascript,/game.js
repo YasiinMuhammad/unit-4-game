40aeb4f07f7f5294a4943d3bd7d79f4fb6f19f7b
Yasiin Muhammad <yasiinmuhammad@Yasiins-MacBook-Pro.local>
2019-01-23 14:34:24 -0700

    init commit

---
+++ assets/javascript,/game.js
@@ -0,0 +1,37 @@
+
+var win = 0;
+var loss = 0;
+var gameResult;
+var playerResults=0;
+
+gameResult = Math.floor(Math.random() * 120 - 12);
+
+$("#gameResults").html('game results: ' + gameResult);
+
+
+for(var i = 0; i < 4; i++) {
+   
+    
+   var randomNum = Math.floor(Math.random() * 12 + 1);
+   
+    
+    var symbol = $("<div>");
+    symbol.attr({
+        "class":'symbol',
+        "data-random": randomNum
+    });
+    $(".symbols").append(symbol);
+
+
+}
+
+// generate randonm # for target score
+// display text $(#yourid).textContent 
+//on click functions for click on crystals
+    //add crystal value to playerResults
+    //call the checkScores function
+
+
+ //function that checks to see if player Value = gameValue if its less than game value it will do nothing if it goes over loose and add to losses, = to win function checkScores () {}
+
+ 
